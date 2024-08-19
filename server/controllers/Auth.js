113a08fba4aee4da10a165d39f0a5b34bb97feb2
Rushikesh <patilrishi272@gmail.com>
2024-08-20 00:15:53 +0530

    last Commit

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -1,7 +1,8 @@
 const User = require("../models/User");
 const OTP = require("../models/OTP");
 const otpGenerator = require("otp-generator");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
+// const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const mailSender = require("../utils/mailSender");
