078880940dee5d1e58ff87cd80790851124e9036
Rushikesh <patilrishi272@gmail.com>
2024-08-20 11:07:05 +0530

    domain name changee

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:4000/api/v1"
+const BASE_URL = "https://study-mart.vercel.app/api/v1";
 
 export const categories = {
   CATEGORIES_API:BASE_URL+ "/course/showAllCategory",
