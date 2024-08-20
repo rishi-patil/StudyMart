609a4039466a3930fa80f6fdf0d4111a15aeefc0
Rushikesh <patilrishi272@gmail.com>
2024-08-20 11:04:00 +0530

    domain name change

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(express.json())
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://study-mart.vercel.app/"], // Replace with your actual Vercel domain
+    origin: "https://study-mart.vercel.app/", // Replace with your actual Vercel domain
     credentials: true,
   })
 );
